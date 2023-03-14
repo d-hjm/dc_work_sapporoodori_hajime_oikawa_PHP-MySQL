74f729e6bad1b98eae346b7ad99986c15fe81dce
d-hjm <h09j2m3@ymail.ne.jp>
2023-03-14 12:11:43 +0900

    Create work02.php

---
+++ htdocs/work/work02.php
@@ -0,0 +1,11 @@
+<?php
+    const height = 180; 		
+    const weight = 65;		
+    
+    print '変更前：身長'.height.'cm 体重'.weight.'kg '; 
+    
+    height = 150;
+    weight = 50;		
+    
+    print '変更後：身長'.height.'cm 体重'.weight.'kg ';
+?>

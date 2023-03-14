cebe258a6f1ee2c55db823bcc28aca559662243a
d-hjm <h09j2m3@ymail.ne.jp>
2023-03-14 15:50:04 +0900

    Update work03.php

---
+++ htdocs/work/work03.php
@@ -0,0 +1,9 @@
+<?php
+    $int = 1; //整数型
+    $float = 0.1; //浮動小数点型
+    $str = "1"; //文字列型
+    $null = null; //NULL型
+    $empty = ''; //空
+
+    print '整数型:'.$int.' 浮動小数点型:'.$float.' 文字列型:'.$str.' NULL型:'.$null.' 空(空文字列):'.$empty;
+?>

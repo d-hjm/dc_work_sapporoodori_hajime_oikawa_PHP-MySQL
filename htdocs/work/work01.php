c2ca1aae94e3fe0e98d04cdf4dade227d53ebd1e
d-hjm <h09j2m3@ymail.ne.jp>
2023-03-14 12:11:38 +0900

    Create work01.php

---
+++ htdocs/work/work01.php
@@ -0,0 +1,19 @@
+<?php
+$height = 158; 		//変数$heightを定義し、値を代入
+const WEIGHT = 44;	//定数WEIGHTを定義し、値を代入
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WORK1</title>
+</head>
+<body>
+<?php
+    print '身長 ';
+    print $height;
+    print 'cm ';
+    print '体重 '.WEIGHT.'kg';
+?>
+</body>
+</html>

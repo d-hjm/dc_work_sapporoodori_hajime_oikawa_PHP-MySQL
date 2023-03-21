87fe98d30c2e51896015d8dc25b666b0a0abbc8b
d-hjm <h09j2m3@ymail.ne.jp>
2023-03-21 11:56:09 +0900

    Update work04.php

---
+++ htdocs/work/work04.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WORK04</title>
+</head>
+<body>
+    <?php
+        $num=(109439032+39443083)/4934; // 「109439032に39443083を加え、4934で割った答え」
+        print '計算の答えは'.$num.'です。';
+    ?>
+</body>
+</html>

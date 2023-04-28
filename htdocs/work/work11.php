02bad6372455ced0d6ce282ce584692ebe38e923
d-hjm <h09j2m3@ymail.ne.jp>
2023-04-28 11:54:04 +0900

    Create work11.php

---
+++ htdocs/work/work11.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WORK11</title>
+</head>
+<body>
+    <?php
+        for($i=1;$i<=100;$i++):
+            if($i%3===0&&$i%4===0):
+                print 'Fizz Buzz';
+            elseif($i%3===0):
+                print 'Fizz';
+            elseif($i%4===0):
+                print 'Buzz';
+            else:
+                print $i;
+            endif;
+        endfor;
+    ?>
+
+    <p>1</p>
+    <?php 
+        for($j=1;$j<=9;$j++):
+            for($k=1;$k<=9;$k++){
+                print '<p>'.$j.'*'.$k.'</p>';
+            }
+        endfor;
+        
+        for($l=1;$l<=10;$l++):
+            for($n=1;$n<=$l;$n++){
+                print '*';
+            }
+            if($l<10):
+                print '<p>!</p>';
+            endif;
+        endfor;
+    ?>
+</body>
+</html>

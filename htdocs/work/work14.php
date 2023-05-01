f87a652c352f46556b1eba1100d3b764687288aa
d-hjm <h09j2m3@ymail.ne.jp>
2023-05-01 12:01:50 +0900

    Create work14.php

---
+++ htdocs/work/work14.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WORK14</title>
+</head>
+<body>
+    <?php
+    $num=array();
+    $random=rand(1,100);
+    for($n=1;$n<=5;$n++){
+        
+    }
+       ?> 
+</body>
+</html>

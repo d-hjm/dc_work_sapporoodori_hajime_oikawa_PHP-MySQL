84d44284e08db8736bc169734b21644e8ae5fa57
d-hjm <h09j2m3@ymail.ne.jp>
2023-05-11 15:53:39 +0900

    Update work15.php

--- htdocs/work/work15.php
+++ htdocs/work/work15.php
@@ -10,12 +10,22 @@
     $class01=["tokugawa","oda","toyotomi","takeda"];
     $class02=["minamoto","taira","sugawara","fujiwara"];
 
-    for($j=0;$j<2;$j++){
+    for($j=1;$j<3;$j++){
         for($i=0;$i<4;$i++){
             $test=rand(1,100);
-            echo "<p>class0".$j."の".$class01[$i]."さんは".$test."点です。</P>";
+            echo "<p>class0".$j."の".${'class0'.$j}[$i]."さんは".$test."点です。</P>"; 
+            ${'class0'.$j}[$i]=$test;
         }
     }
+
+    if($school[0][1]>$school[1][2]){
+        echo "<p>odaさんの方が点数が高いです。</P>";
+    }else{
+        echo "<p>sugawaraさんの方が点数が高いです。</P>";
+    }
+
+
+    // for($k=0;$k<8;)
     ?>
 
 </body>

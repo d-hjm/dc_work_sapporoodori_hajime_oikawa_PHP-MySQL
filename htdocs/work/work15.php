d8a30af2003a144e0fefbdce77e3e92dfe600b20
d-hjm <h09j2m3@ymail.ne.jp>
2023-05-15 14:07:18 +0900

    Update work15.php

--- htdocs/work/work15.php
+++ htdocs/work/work15.php
@@ -8,34 +8,21 @@
     <?php
     $class01=["tokugawa","oda","toyotomi","takeda"];
     $class02=["minamoto","taira","sugawara","fujiwara"];
-    $school=[$class01,$class02];
-
-    // $test=rand(1,100);
-    // echo "<p>class01の".$class01[0]."さんは".$test."点です。</P>";
-    // $class01[0]=$test;
+    $school=array($class01,$class02);
 
     for($j=1;$j<3;$j++){
         for($i=0;$i<4;$i++){
             $test=rand(1,100);
             echo "<p>class0".$j."の".${'class0'.$j}[$i]."さんは".$test."点です。</P>"; 
             ${'class0'.$j}[$i]=$test;
+            $school[$j-1][$i]=$test;
         }
     }
-
-    <pre>
-    echo "<p>".$school[0][1]."</P>";
-    print_r($school);
-
     if($school[0][1]>$school[1][2]){
         echo "<p>odaさんの方が点数が高いです。</P>";
-    }else if($school[0][1]<$school[1][2]){
-        echo "<p>sugawaraさんの方が点数が高いです。</P>";
     }else{
-        
+        echo "<p>sugawaraさんの方が点数が高いです。</P>";
     }
-
-
-    // for($k=0;$k<8;)
     ?>
 
 </body>

004721535adb7b60d6cebad02e10107cf560aecd
d-hjm <h09j2m3@ymail.ne.jp>
2023-05-18 15:58:07 +0900

    Update work16_02.php

--- htdocs/work/work16_02.php
+++ htdocs/work/work16_02.php
@@ -5,23 +5,33 @@
         <title>WORK16</title>
     </head>
     <body>
-        <?php
-            if( isset( $_GET['name_text'] ) && isset( $_GET['check[]'] )) {
-                print  htmlspecialchars($_GET['name_text']."は".$_GET['check[]']."です。", ENT_QUOTES, 'UTF-8');
-            }else if(isset( $_GET['name_text'] )&&isset( $_GET['check[]'] )===null){
+        <?php 
+            $name = htmlspecialchars($_GET['name_text'], ENT_QUOTES, 'UTF-8');
+            $check = $_GET["check"];
+            $checkbox=array();
+            if($name != "" && isset($check)) {
+                print  $name."は";
+                for($i = 0;$i < count($check);$i++){
+                    print $check[$i];
+                    array_push($checkbox,$check[$i]);
+                }
+                print "です。";
+            }else if($name != ""){
                 print 'チェックボックスにチェックを入れてください。';
-            }else if(isset( $_GET['name_text'] )===""&&isset( $_GET['check[]'] )){
+            }else if(isset($check)){
                 print '名前を入力してください';
             }else{
                 print '何か入力して…';
             }
         ?>
         <pre>
-            <?php 
-            print_r($_GET);
+            <?php
+            echo "<p>".print_r($checkbox);."</p>";
+            print_r(getAllCombinations($check));
             ?>
         </pre>
-            
+        <?php 
+            var_dump($check);
         ?>
     </body>
 </html>

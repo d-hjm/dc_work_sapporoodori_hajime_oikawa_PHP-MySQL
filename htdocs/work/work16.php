32622b39d097c4d9fb32650aeed2d91bb4ca03cc
d-hjm <h09j2m3@ymail.ne.jp>
2023-05-18 15:58:04 +0900

    Update work16.php

--- htdocs/work/work16.php
+++ htdocs/work/work16.php
@@ -8,13 +8,11 @@
         <div>名前を書いてください</div>
         <form method="get" action="work16_02.php">
             <input type="text" name="name_text">
-        <div>彼(彼女、または自分)は…</div>
-            <input type="checkbox" name="check[]">グッド</input>
-            <input type="checkbox" name="check[]">グレート</input>
-            <input type="checkbox" name="check[]">うわー！！！</input>
+            <div>彼(彼女、または自分)は…</div>
+            <input type="checkbox" name="check[]" value="グッド">グッド</input>
+            <input type="checkbox" name="check[]" value="グレード">グレート</input>
+            <input type="checkbox" name="check[]" value="うわー！">うわー！！！</input>
             <input type="submit" value="送信">(複数回答可)</input>
         </form>
     </body>
 </html>
-
-$_GET={name,check[0,1,2]}

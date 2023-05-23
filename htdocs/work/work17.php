de82c74fbe0e156ee5b0d910b2c485483e5dd7bb
d-hjm <h09j2m3@ymail.ne.jp>
2023-05-23 13:37:16 +0900

    Update work17.php

--- htdocs/work/work17.php
+++ htdocs/work/work17.php
@@ -6,13 +6,39 @@
     </head>
     <body>
         <div>名前を書いてください</div>
-        <form method="get" action="work16_02.php">
+        <form method="post">
             <input type="text" name="name_text">
             <div>彼(彼女、または自分)は…</div>
-            <input type="checkbox" name="check[]" value="グッド">グッド</input>
-            <input type="checkbox" name="check[]" value="グレード">グレート</input>
-            <input type="checkbox" name="check[]" value="うわー！">うわー！！！</input>
+            <input type="checkbox" name="check[]" value="グッド">グッド
+            <input type="checkbox" name="check[]" value="グレート">グレート
+            <input type="checkbox" name="check[]" value="うわー！">うわー！！！
             <input type="submit" value="送信">(複数回答可)</input>
         </form>
+        <?php
+            $name = htmlspecialchars($_POST['name_text'], ENT_QUOTES, 'UTF-8');
+            $check = $_POST["check"];
+            $checkbox=array();
+            if($_SERVER["REQUEST_METHOD"] == "POST"){
+                if($name != "" && isset($check)) {
+                    print  $name."は";
+                    for($i = 0;$i < count($check);$i++){
+                        print $check[$i];
+                        array_push($checkbox,$check[$i]);
+                    }
+                    print "です。";
+                }else if($name != ""){
+                    print 'チェックボックスにチェックを入れてください。';
+                }else if(isset($check)){
+                    print '名前を入力してください';
+                }else{
+                    print '何か入力して…';
+                }
+            }
+        ?>
+         <pre>
+            <?php
+            echo print_r($checkbox);
+            ?>
+        </pre>
     </body>
 </html>

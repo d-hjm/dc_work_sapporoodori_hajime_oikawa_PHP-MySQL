bf9925c16a42720c5d4ba8d638527018a3bc421c
d-hjm <h09j2m3@ymail.ne.jp>
2023-06-09 15:38:41 +0900

    Update work19.php

--- htdocs/work/work19.php
+++ htdocs/work/work19.php
@@ -1,3 +1,20 @@
+<?php
+    $shortage="";
+    $title = $_POST["title"];
+    $content = $_POST["content"];
+    if($title == "" || $content == ""){
+        $shortage="入力情報が不足しています";
+    }else{
+        $fp_w=fopen("read&write.txt","a");
+        fwrite($fp_w,$title.":".$content."\n");
+        // ftruncate($fp_w,0);
+        fclose($fp_w);
+    }
+    $data = file_get_contents("read&write.txt");
+    $data = explode( "\n", $data);
+    $array = array_reverse($data);
+    $cnt = count($array);
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,10 +22,8 @@
     <title>WORK19</title>
 </head>
 <body>
-    <?php $shortage="" ?>
-
     <form method="post">
-            <div><p> <?php echo $shortage; ?> </div>
+            <p> <?php echo $shortage ?> </p>
             <div>タイトル</div>
             <input type="text" name="title">
             <br>
@@ -19,40 +34,10 @@
             <input type="submit" value="送信">
             <br>
     </form>
-    
-    <?php
-        $title = $_POST["title"].":";
-        $content = $_POST["content"]."\n";
-
-        $fp_w=fopen("read&write.txt","a");
-        fwrite($fp_w,$title.$content);
-
-        $data = file_get_contents("read&write.txt");
-        $data = explode( "\n", $data);
-        $cnt = count($data);
-        $array = array_reverse($data);
-    ?>
-        
-    <?php if($title !== ""&&$content !== ""):
-            for($i=0;$i<$cnt;$i++): ?>
-                <ul>
-                    <li> <?php echo "$array[$i]<br>"; ?> </li>
-                </ul>
-                <?php endfor; ?>
-    <?php else:
-            $shortage="入力情報が不足しています<br>";
-           
-            for($i=0;$i<$cnt;$i++): ?> 
-                <ul>
-                    <li> <?php echo "$array[$i]<br>"; ?> </li>
-                </ul>
-                <?php endfor; ?>
-    <?php endif; ?>
-        
-    <?php
-        ftruncate($fp_w,0);
-        print_r($array);
-        fclose($fp_w);
-    ?>
+    <ul>
+        <?php for($i=1;$i<$cnt;$i++):?>
+                <li> <?php echo $array[$i]."<br>"; ?> </li>
+        <?php endfor; ?>
+    </ul>
 </body>
 </html>

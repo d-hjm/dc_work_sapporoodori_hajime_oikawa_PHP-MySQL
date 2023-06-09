670f4c3cc057645ae0d0a2460f488661077e69ae
d-hjm <h09j2m3@ymail.ne.jp>
2023-06-09 13:23:59 +0900

    Update work19.php

--- htdocs/work/work19.php
+++ htdocs/work/work19.php
@@ -8,7 +8,7 @@
     <?php $shortage="" ?>
 
     <form method="post">
-            <div><?php echo $shortage; ?></div>
+            <div><p> <?php echo $shortage; ?> </div>
             <div>タイトル</div>
             <input type="text" name="title">
             <br>
@@ -21,33 +21,36 @@
     </form>
     
     <?php
-        $title = $_POST["title"];
+        $title = $_POST["title"].":";
         $content = $_POST["content"]."\n";
 
         $fp_w=fopen("read&write.txt","a");
-        fwrite($fp_w,$title.":".$content);
+        fwrite($fp_w,$title.$content);
 
         $data = file_get_contents("read&write.txt");
         $data = explode( "\n", $data);
         $cnt = count($data);
         $array = array_reverse($data);
     ?>
-        <ul>
+        
     <?php if($title !== ""&&$content !== ""):
             for($i=0;$i<$cnt;$i++): ?>
+                <ul>
                     <li> <?php echo "$array[$i]<br>"; ?> </li>
-                <?php endfor;
-        else:
+                </ul>
+                <?php endfor; ?>
+    <?php else:
             $shortage="入力情報が不足しています<br>";
-            array_splice($array,0);
+           
             for($i=0;$i<$cnt;$i++): ?> 
+                <ul>
                     <li> <?php echo "$array[$i]<br>"; ?> </li>
-                <?php endfor;
-        endif;
-    ?>
-        </ul>
+                </ul>
+                <?php endfor; ?>
+    <?php endif; ?>
+        
     <?php
-        // ftruncate($fp_w,0);
+        ftruncate($fp_w,0);
         print_r($array);
         fclose($fp_w);
     ?>
